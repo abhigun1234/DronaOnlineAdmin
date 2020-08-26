d77b0dc4f5f7d318bfbfaeef9f7f9168bae47c8c
Gungun <gungun@Gunguns-MacBook-Pro.local>
2020-08-26 20:52:36 +0530

    sms

--- institute/views.py
+++ institute/views.py
@@ -71,6 +71,28 @@ class RegisterUser(APIView):
                 country=userData['country']
                 email=userData['email']
                 birth_date=userData['birth_date']
+                # Python code to illustratdde Sending mail from
+                # your Gmail account
+                print(request.json)
+                import smtplib
+
+                # creates SMTP session
+                s = smtplib.SMTP('smtp.gmail.com', 587)
+                s.ehlo()
+                # start TLS for security
+                s.starttls()
+                s.ehlo()
+                # Authentication
+                s.login("justolearnpune@gmail.com", "justo.007")
+
+                # message to be sent
+                message = "Message_you_need_to_send"
+
+                # sending the mail
+                s.sendmail("nitesh.aryan007@gmail.com", "nitesh.aryan007@gmail.com", message)
+
+                # terminating the session
+                s.quit()
                 user=User(name=name,phone_no=phone_no,city=city,email=email,birth_date=birth_date)
                 user.save()
                 response='registerd'
