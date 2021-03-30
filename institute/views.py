62fa67192a19d6ba1255e49c71bcf7c4a2b3f64e
Abhishek <abhi.ghosh27feb@gmail.com>
2021-03-30 20:23:30 +0530

    add

--- institute/views.py
+++ institute/views.py
@@ -58,82 +58,20 @@ class RegisterUser(APIView):
         return Response({'userdetails': serilezer.data})
 
     def post(self,request):
-
         print(request.data)
         userData=request.data
         response=''
-
-
-        # name=userData['name']
-        # print(name)
-        # phone_no=userData['phone_no']
-        # city=userData['city']
-        # country=userData['country']
-        # email=userData['email']
-        # birth_date=userData['birth_date']
-        # # Python code to illustratdde Sending mail from
-        # # your Gmail account
-        # print(request.json)
-        # import smtplib
-        #
-        # # creates SMTP session
-        # s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        # s.ehlo()
-        # # start TLS for security
-        # s.starttls()
-        # s.ehlo()
-        # # Authentication
-        # s.login("justolearnpune@gmail.com", "justo.007")
-        #
-        # # message to be sent
-        # message = "new enquiry"
-        #
-        # # sending the mail
-        # s.sendmail("justolearnpune@gmail.com", "justolearnpune@gmail.com", message)
-        #
-        # # terminating the session
-        # s.quit()
-        # #user=User(name=name,phone_no=phone_no,city=city,email=email,birth_date=birth_date)
-        # #user.save()
-        # response='registerd'
-        # return Response(response)
-        # except :
-        #         response = 'error'
-        #         return Response(response)
-        import smtplib
-
-        gmail_user = 'justolearnpune@gmail.com'
-        gmail_password = 'justo.007'
-
-        sent_from = gmail_user
-        to = ['justolearnpune.com', 'justolearnpune.com']
-        subject = 'OMG Super Important Message'
-        body = 'Hey, what'
-
-        email_text = """\
-        From: %s
-        To: %s
-        Subject: %s
-
-        %s
-        """ % (sent_from, ", ".join(to), subject, body)
-
-        # try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.ehlo()
-        server.starttls()
-        server.login('justolearnpune@gmail.com', 'justo.007')
-        server.sendmail('justolearnpune@gmail.com', 'justolearnpune@gmail.com', 'hello')
-        server.close()
-        #
-        # print ('Email sent!')
+        name=userData['name']
+        print(name)
+        phone_no=userData['phone_no']
+        city=userData['city']
+        country=userData['country']
+        email=userData['email']
+        birth_date=userData['birth_date']
+        user=User(name=name,phone_no=phone_no,city=city,email=email,birth_date=birth_date)
+        user.save()
         response='registerd'
         return Response(response)
-        # except:
-        #     response = 'error'
-        #     return Response(response)
-        #     print('Something went wrong...')
-
 
 class UserLoginApiView(APIView):
     permission_classes = []
